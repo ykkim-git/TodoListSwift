2155426f6b72bb057b25693213a5656a4fc3d38b
jay <rladudrnjs12@gmail.com>
2022-12-03 22:36:06 +0900

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Lorenzo Von Matterhorn on 2022/12/03.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
